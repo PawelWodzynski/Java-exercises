6ec2a238b60dc4aabbd12188c57f1f4d9d64d393
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-04-11 00:02:26 +0200

    HashSet

---
+++ HashSet/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<String> posilki = new HashSet<>();     // w HashSet nie możliwe są duplikaty
+
+        posilki.add("Zupa");
+        posilki.add("Cziken");
+        posilki.add("Ramen");
+
+       // System.out.println(posilki);   // wyświetli w ten sposób posiłki - [Twoja stara z rożna, Zupa, Cziken]
+
+        for ( String posilek : posilki) {
+            System.out.println(posilek);           // for wyświetli je pod sobą
+        }
+
+    }
+}
