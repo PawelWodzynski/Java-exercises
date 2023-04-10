6a342ca908c362865e44544747d87ece64e83820
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-04-11 00:09:19 +0200

    Bidding app for Allegro.pl

---
+++ Bidding_app/src/MouseClicker.java
@@ -0,0 +1,39 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MouseClicker {
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int x = 1600;
+        int y = 550;
+
+        //  godzina wykonania kliknięcia
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 51);
+        calendar.set(Calendar.SECOND, 0);
+        Date executionTime = calendar.getTime();
+
+        // oczekiwanie na godzine
+        while (new Date().before(executionTime)) {
+            Thread.sleep(1);
+        }
+
+
+        Robot robot = new Robot();
+        robot.mouseMove(x, y);
+//        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        int licytacja_x = 1600;
+                int licytacja_y=620;
+
+
+        robot.mouseMove(licytacja_x, licytacja_y);
+//        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+    }
+}
