8b121da8f7b0fb4b3e3bc894b1e78019a1d086dc
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-04-10 23:48:20 +0200

    array of string

---
+++ Array_of_string/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Jak można nazwać głupiego człowieka?");
+
+
+
+
+//        String[] zwierzeta = new String[4];
+//       zwierzeta[0] = "Kot";
+//       zwierzeta[1] = "Pies";
+//       zwierzeta[2] = "Chomik";
+//       zwierzeta[3] = "Królik";
+//
+//       for (int i = 0; i< zwierzeta.length; i++) {
+//           System.out.println(zwierzeta[i]);
+//       }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+         String[] zwierzeta = {"Goryl","Szympans","Kapucynka","Gibon", "Orangutan"};
+
+         for (String zwierze : zwierzeta) {
+             System.out.println(zwierze);
+         }
+
+
+
+
+
+
+    }
+}
